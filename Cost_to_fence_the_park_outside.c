2410239c7291a26772540f01ec5e8f4161089888
22P31A0404 <119718714+22P31A0404@users.noreply.github.com>
2022-12-30 10:04:29 +0530

    #include<stdio.h> int main() {     int l,b,w,c,tc,a1,a2,a3;     scanf("%d%d%d%d",&l,&b,&w,&c);     a1=l*b;     a2=(l+2*w)*(b+2*w);     a3=a2-a1;     tc=c*a3;     printf("%d",tc); }

---
+++ Cost_to_fence_the_park_outside.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int l,b,w,c,tc,a1,a2,a3;
+    scanf("%d%d%d%d",&l,&b,&w,&c);
+    a1=l*b;
+    a2=(l+2*w)*(b+2*w);
+    a3=a2-a1;
+    tc=c*a3;
+    printf("%d",tc);
+}
