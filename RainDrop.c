7b2719b057c9952015eee81a4a1d9adb039fa9c5
22P31A0404 <119718714+22P31A0404@users.noreply.github.com>
2023-01-08 18:32:33 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     if(n%3==0 && n%5==0 && n%7!=0)     {         printf("PlingPlang");     }     else if(n%3!=0 && n%5!=0 && n%7==0)     {         printf("Plong");     }     else if(n%3==0 && n%5!=0 && n%7!=0)     {         printf("Pling");     }     else if(n%3!=0 && n%5==0 && n%7!=0)     {         printf("Plang");     }     else if(n%3!=0 && n%5!=0 && n%7==0)     {         printf("Plong");     }     else     {         printf("%d",n);     } }

---
+++ RainDrop.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n%3==0 && n%5==0 && n%7!=0)
+    {
+        printf("PlingPlang");
+    }
+    else if(n%3!=0 && n%5!=0 && n%7==0)
+    {
+        printf("Plong");
+    }
+    else if(n%3==0 && n%5!=0 && n%7!=0)
+    {
+        printf("Pling");
+    }
+    else if(n%3!=0 && n%5==0 && n%7!=0)
+    {
+        printf("Plang");
+    }
+    else if(n%3!=0 && n%5!=0 && n%7==0)
+    {
+        printf("Plong");
+    }
+    else
+    {
+        printf("%d",n);
+    }
+}
